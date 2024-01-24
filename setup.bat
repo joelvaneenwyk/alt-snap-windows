f9cf26de0fab922264b5c17b77463f7c29f7c299
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-01-23 16:55:01 -0800

    Add more dependencies to 'setup.bat'

--- setup.bat
+++ setup.bat
@@ -1,8 +1,30 @@
 @echo off
-setlocal EnableDelayedExpansion
+goto:$Main
 
-:: https://www.mingw-w64.org/downloads/#mingw-builds
-call scoop install "main/mingw"
-call scoop install "main/mingw-winlibs"
-call scoop install "main/rhash"
-call scoop install "main/7zip"
+:Scoop
+    echo ##[cmd] scoop %*
+    call scoop %*
+exit /b %ERRORLEVEL%
+
+:$Main
+    setlocal EnableExtensions
+
+    :: https://www.mingw-w64.org/downloads/#mingw-builds
+    call :Scoop bucket add "main"
+    call :Scoop install "main/mingw"
+    call :Scoop install "main/mingw-winlibs"
+    call :Scoop install "main/rhash"
+    call :Scoop install "main/7zip"
+    call :Scoop install "main/tcc"
+    call :Scoop install "main/ninja"
+    call :Scoop install "main/msys2"
+
+    call :Scoop bucket add "versions"
+    call :Scoop install "versions/mingw-winlibs-ucrt"
+    call :Scoop install "versions/mingw-winlibs-ucrt-mcf"
+    call :Scoop install "versions/mingw-winlibs-llvm"
+    call :Scoop install "versions/mingw-winlibs-llvm-ucrt"
+    call :Scoop install "versions/mingw-winlibs-llvm-ucrt-mcf"
+
+    call :Scoop update *
+endlocal & goto:EOF
