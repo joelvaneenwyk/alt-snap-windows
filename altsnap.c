0479aaabc7eae6af1a04eb878003106cdc357d02
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-01-26 13:55:57 -0800

    Remove new and superfluous 'framework.h' and 'targetver.h'
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- altsnap.c
+++ altsnap.c
@@ -666,8 +666,24 @@ void noreturn WINAPI unfuckWinMain(void)
 //
 
 #include "AltSnap.h"
-#include "framework.h"
 
+
+// Including SDKDDKVer.h defines the highest available Windows platform.
+// ---------------------------------------------------------------------------
+// If you wish to build your application for a previous Windows platform, include WinSDKVer.h and
+// set the _WIN32_WINNT macro to the platform you wish to support before including SDKDDKVer.h.
+#include <SDKDDKVer.h>
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+
+// Windows Header Files
+#include <windows.h>
+
+// C RunTime Header Files
+#include <malloc.h>
+#include <memory.h>
+#include <stdlib.h>
+#include <tchar.h>
 
 #define MAX_LOADSTRING 100
 
