890fdf5229c988f46035d47244e88505ace95972
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-01-26 14:05:27 -0800

    Install makensis during setup
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- setup.bat
+++ setup.bat
@@ -7,24 +7,26 @@ goto:$Main
 exit /b %ERRORLEVEL%
 
 :$Main
-    setlocal EnableExtensions
+setlocal EnableExtensions
+    call pwsh -NoProfile -Command "iwr -useb get.scoop.sh | iex"
+    call npm install -g makensis
 
     :: https://www.mingw-w64.org/downloads/#mingw-builds
     call :Scoop bucket add "main"
-    call :Scoop install "main/mingw"
-    call :Scoop install "main/mingw-winlibs"
-    call :Scoop install "main/rhash"
-    call :Scoop install "main/7zip"
-    call :Scoop install "main/tcc"
-    call :Scoop install "main/ninja"
-    call :Scoop install "main/msys2"
+    call :Scoop install --no-update-scoop "main/mingw"
+    call :Scoop install --no-update-scoop "main/mingw-winlibs"
+    call :Scoop install --no-update-scoop "main/rhash"
+    call :Scoop install --no-update-scoop "main/7zip"
+    call :Scoop install --no-update-scoop "main/tcc"
+    call :Scoop install --no-update-scoop "main/ninja"
+    call :Scoop install --no-update-scoop "main/msys2"
 
     call :Scoop bucket add "versions"
-    call :Scoop install "versions/mingw-winlibs-ucrt"
-    call :Scoop install "versions/mingw-winlibs-ucrt-mcf"
-    call :Scoop install "versions/mingw-winlibs-llvm"
-    call :Scoop install "versions/mingw-winlibs-llvm-ucrt"
-    call :Scoop install "versions/mingw-winlibs-llvm-ucrt-mcf"
+    call :Scoop install --no-update-scoop "versions/mingw-winlibs-ucrt"
+    call :Scoop install --no-update-scoop "versions/mingw-winlibs-ucrt-mcf"
+    call :Scoop install --no-update-scoop "versions/mingw-winlibs-llvm"
+    call :Scoop install --no-update-scoop "versions/mingw-winlibs-llvm-ucrt"
+    call :Scoop install --no-update-scoop "versions/mingw-winlibs-llvm-ucrt-mcf"
 
     call :Scoop update *
 endlocal & goto:EOF
